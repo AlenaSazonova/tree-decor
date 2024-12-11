82a8674386577f8fba07b716bf99b346c652497f
“AlenaSazonava” <alenka03.92@mail.ru>
2024-12-11 17:13:36 +0300

    feat: added data and music functionality to the TreeDecorator component

--- src/components/pages/TreeDecorator/TreeDecorator.js
+++ src/components/pages/TreeDecorator/TreeDecorator.js
@@ -1,17 +1,140 @@
-import React from 'react';
+import React, { useRef, useState, useEffect } from 'react';
 import Header from '../../Header/Header';
+import mute from '../../icons/svg/mute.svg';
+import snow from '../../icons/svg/snow.svg';
+import { initialState } from '../../store/reducers/GeneralReducer';
+import audioMP3 from '../../audio/audio.mp3';
+
+
+
 
 import {
-    Container
+    Container,
+    ContainerForContent,
+    DecoratorWrapper,
+    ControlSection,
+    ButtonMuteImg,
+    ButtonSnowImg,
+    MuteImg,
+    SnowImg,
+    TreeSelectionSection,
+    TreeButtonImgContainer,
+    TreeButton,
+    TreeImg,
+    BackgroundSelectionSection,
+    BackgroundButtonImgContainer,
+    BackgroundImg,
+    GarlandSelectionSection,
+    GarlandButtonImgContainer,
+    GarlandImg,
+    ButtonSection,
+    SaveButton,
+    ResetButton
 }
     from './TreeDecorator.style';
 
 const TreeDecorator = () => {
+    const [audioStatus, setAudioStatus] = useState(false)
+    const tree = initialState.tree;
+    const background = initialState.background;
+    const garland = initialState.garland;
+    const audioRef = useRef(new Audio(audioMP3));
+
+
+    const startAudio = () => {
+        audioRef.current.play();
+        setAudioStatus(true);
+    }
+
+    const pauseAudio = () => {
+        audioRef.current.pause();
+        setAudioStatus(false);
+    }
+
+    useEffect(() => {
+        const audio = audioRef.current;
+
+        return () => {
+            audio.pause();
+            audio.currentTime = 0;
+        }
+    }, [])
+
 
     return (
         <Container>
             <Header />
-            <p>Trees</p>
+            <ContainerForContent>
+                <DecoratorWrapper>
+                    <ControlSection>
+                        <ButtonMuteImg onClick={audioStatus ? pauseAudio : startAudio}>
+                            <MuteImg src={mute} alt={audioStatus ? "pause" : "start"} />
+                        </ButtonMuteImg>
+                        <ButtonSnowImg>
+                            <SnowImg src={snow} alt="snow" />
+                        </ButtonSnowImg>
+                    </ControlSection>
+
+                    <TreeSelectionSection>
+                        <h5>Выберите елку</h5>
+                        <TreeButtonImgContainer>
+                            {tree.map((el, ind) => (
+                                <TreeButton key={ind}>
+                                    <TreeImg
+                                        src={el.src}
+                                        alt={el.alt}
+                                    />
+                                </TreeButton>
+                            ))}
+                        </TreeButtonImgContainer>
+                    </TreeSelectionSection>
+
+                    <BackgroundSelectionSection>
+                        <h5>Выберите фон</h5>
+                        <BackgroundButtonImgContainer>
+                            {background.map((el, ind) => (
+                                <button key={ind}>
+                                    <BackgroundImg
+                                        src={el.src}
+                                        alt={el.alt}
+                                    />
+                                </button>
+                            ))}
+                        </BackgroundButtonImgContainer>
+                    </BackgroundSelectionSection>
+
+                    <GarlandSelectionSection>
+                        <h5>Гирлянда</h5>
+                        <GarlandButtonImgContainer>
+                            {garland.map((el, ind) => (
+                                <button key={ind}>
+                                    <GarlandImg
+                                        src={el.src}
+                                        alt={el.alt}
+                                    />
+                                </button>
+                            ))}
+                        </GarlandButtonImgContainer>
+                    </GarlandSelectionSection>
+
+                    <ButtonSection>
+                        <SaveButton>Сохранить</SaveButton>
+                        <ResetButton>Сбросить</ResetButton>
+                    </ButtonSection>
+                </DecoratorWrapper>
+
+
+
+
+
+                <div>
+                    <div>empty</div>
+                </div>
+
+                <div>
+                    <div>empty</div>
+                </div>
+            </ContainerForContent>
         </Container>
     );
 };
--- src/components/pages/TreeDecorator/TreeDecorator.style.js
+++ src/components/pages/TreeDecorator/TreeDecorator.style.js
@@ -4,3 +4,127 @@ import styled from "styled-components";
 export const Container = styled.div`
     
 `;
+
+export const ContainerForContent = styled.div`
+    display: flex;
+`;
+
+export const DecoratorWrapper = styled.div`
+    width: 440px;
+`;
+
+export const ControlSection = styled.div`
+    margin-bottom: 51px;
+`;
+
+export const ButtonMuteImg = styled.button`
+    margin-right: 40px;
+`;
+
+export const ButtonSnowImg = styled.button`
+`;
+
+export const MuteImg = styled.img`
+    width: 36px;
+    height: 36px;
+`;
+
+export const SnowImg = styled.img`
+    width: 36px;
+    height: 36px;
+`;
+
+export const TreeSelectionSection = styled.div`
+    margin-bottom: 36px;
+`;
+
+export const TreeButtonImgContainer = styled.div`
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+    gap: 16px;
+`;
+
+export const TreeImg = styled.img`
+    width: 91px;
+    height: 120px;
+`;
+
+export const TreeButton = styled.button`
+    flex: 1 1 33.33%;
+    max-width: 134px;
+`;
+
+export const BackgroundSelectionSection = styled.div`
+    margin-bottom: 36px;
+`;
+
+export const BackgroundButtonImgContainer = styled.div`
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+    gap: 16px;
+`;
+
+export const BackgroundImg = styled.img`
+    width: 68px;
+    height: 68px;
+`;
+
+export const GarlandSelectionSection = styled.div`
+    margin-bottom: 36px;
+`;
+
+export const GarlandButtonImgContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 20px;
+`;
+
+export const GarlandImg = styled.img`
+    width: 284px;
+`;
+
+export const ButtonSection = styled.div`
+`;
+
+export const SaveButton = styled.button`
+    border-radius: 20px;
+    max-width: 150px;
+    width: 100%;
+    height: 50px;
+    background: #2d5d20;
+    font-weight: 700;
+    font-size: 20px;
+    line-height: 247%;
+    text-align: center;
+    color: #fff;
+    cursor: pointer;
+    transition: background 0.3s;
+    margin-left: 50px;
+
+    &:hover {
+        background: #4b8b3a;
+    }
+`;
+
+export const ResetButton = styled.button`
+border-radius: 20px;
+    max-width: 150px;
+    width: 100%;
+    height: 50px;
+    background: #2d5d20;
+    font-weight: 700;
+    font-size: 20px;
+    line-height: 247%;
+    text-align: center;
+    color: #fff;
+    cursor: pointer;
+    transition: background 0.3s;
+    margin-left: 50px;
+
+    &:hover {
+        background: #4b8b3a;
+    }
+`;
