7c89e99e7679fff61e6297721502d268aceade72
“AlenaSazonava” <alenka03.92@mail.ru>
2025-04-15 16:39:28 +0300

    chore: migrate index.js to TypeScript

--- src/index.tsx
+++ src/index.tsx
@@ -7,7 +7,7 @@ import store from './components/store/store';
 import reportWebVitals from './reportWebVitals';
 
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 
 root.render(
     <Provider store={store}>
